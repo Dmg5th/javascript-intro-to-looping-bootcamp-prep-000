e96b56eb31b98b5dbbe0431f7d59cb0b03a6b5c5
Daniel Gould <dmgould310@gmail.com>
2020-07-22 14:49:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,24 +1,28 @@
 function forLoop(array){
-  for (let i = 0; i<25; i++){
-    if (i === 1) {
-      array.push(`I am 1 strange loop.`)
-    } else {
-      array.push(`I am ${i} strange loops.`)
-    }
+for(let i = 0; i < 25; i++){
+   if(i === 1){
+    array.push("I am 1 strange loop.")
+   } else {
+     array.push(`I am ${i} strange loops.`)
+   }
   }
   return array
 }
+
 function whileLoop(number){
-  let i = number
-  while(i>0){
-    console.log(--i)
+  let i = 0;
+  while(i < 25) {
+    console.log(number)
+    number --
   }
-  return "done";
+  return "done"
 }
 
-function doWhileLoop(integer) {
-  var i = 1;
-  do{
-    console.log("I run once regardless.");
-  }while(i++ < integer);
-}
+
+
+
+
+
+
+
+
